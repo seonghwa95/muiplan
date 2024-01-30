e9eb108c9d08a2692fc54b9a8123ae9b84590276
seonghwa95 <shinholy@shinholyui-MacBookPro.local>
2024-01-31 00:02:20 +0900

    initial commit

---
+++ src/main/java/com/shop/muiplan/MuiplanApplication.java
@@ -0,0 +1,13 @@
+package com.shop.muiplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuiplanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuiplanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/muiplan/controller/PostControllerTest.java
@@ -0,0 +1,15 @@
+package com.shop.muiplan.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest {
+
+    @Test
+    @DisplayName("/posts 요청시 Hello MUIPLAN!을 출력한다.")
+    void test() {
+
+    }
+}
