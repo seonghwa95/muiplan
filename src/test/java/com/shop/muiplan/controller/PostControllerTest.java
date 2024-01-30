82bd3d0cc5b0654bde092e30e17536cf757e59cc
seonghwa95 <shinholy@shinholyui-MacBookPro.local>
2024-01-31 00:17:16 +0900

    Test: PostControllerTest

--- src/test/java/com/shop/muiplan/controller/PostControllerTest.java
+++ src/test/java/com/shop/muiplan/controller/PostControllerTest.java
@@ -2,14 +2,27 @@ package com.shop.muiplan.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WebMvcTest
 class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     @DisplayName("/posts 요청시 Hello MUIPLAN!을 출력한다.")
-    void test() {
-
+    void test() throws Exception {
+        mockMvc.perform(get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello MUIPLAN!"))
+                .andDo(print()); // summary 확인
     }
 }
